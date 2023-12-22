3757d7949db662a5782d45642484dd018a8c23d8
Joinemm <joonas@rautiola.co>
2023-12-22 14:33:40 +0200

    Add nginx module

--- modules/default.nix
+++ modules/default.nix
@@ -1,3 +1,4 @@
+# Reusable NixOS modules
 {
   laptop = import ./laptop.nix;
   common = import ./common.nix;
@@ -6,4 +7,5 @@
   syncthing = import ./syncthing.nix;
   work-vpn = import ./work-vpn.nix;
   ssh-access = import ./ssh-access.nix;
+  nginx = import ./nginx.nix;
 }
