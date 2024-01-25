5c9cf377216194dddb138307402209ddc0f38a35
Joinemm <joonas@rautiola.co>
2024-01-25 20:23:57 +0200

    Use amdgpu driver

--- hosts/zeus/configuration.nix
+++ hosts/zeus/configuration.nix
@@ -75,14 +75,9 @@ in {
   programs.gamemode.enable = true;
 
   # Add opengl/vulkan support
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [
-      libva
-    ];
-  };
+  hardware.opengl.extraPackages = with pkgs; [
+    libva
+  ];
 
   hardware.pulseaudio.daemon.config = {
     default-sample-format = "float32le";
@@ -96,7 +91,7 @@ in {
 
     xrandrHeads = [
       {
-        output = "DP-1";
+        output = "DisplayPort-0";
         primary = true;
         monitorConfig = ''
           Modeline "3440x1440_144.00"  1086.75  3440 3744 4128 4816  1440 1443 1453 1568 -hsync +vsync
@@ -104,6 +99,8 @@ in {
         '';
       }
     ];
+
+    videoDrivers = ["amdgpu"];
   };
 
   environment.systemPackages = lib.flatten [
