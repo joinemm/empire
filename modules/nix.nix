a0bc8d55ed9bdc1d910c4aa982c048d58e3f9b36
Joinemm <joonas@rautiola.co>
2024-05-02 13:03:59 +0300

    Remove numtide cache to avoid trusted key issues

--- modules/nix.nix
+++ modules/nix.nix
@@ -10,12 +10,10 @@
   nix = {
     settings = {
       substituters = [
-        "https://numtide.cachix.org"
         "https://nix-gaming.cachix.org"
         "https://cache.vedenemo.dev"
       ];
       trusted-public-keys = [
-        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "cache.vedenemo.dev:8NhplARANhClUSWJyLVk4WMyy1Wb4rhmWW2u8AejH9E="
       ];
