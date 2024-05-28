9a9367aa559878154037e9f8fa47cdda300d34d6
Joinemm <joonas@rautiola.co>
2024-05-28 22:09:22 +0300

    Use Finnish locale

--- modules/locale.nix
+++ modules/locale.nix
@@ -1,6 +1,12 @@
 {
   time.timeZone = "Europe/Helsinki";
-  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n = {
+    defaultLocale = "fi_FI.UTF-8";
+    extraLocaleSettings = {
+      LC_MESSAGES = "en_US.UTF-8";
+    };
+  };
 
   location.provider = "geoclue2";
   services.geoclue2.enable = true;
