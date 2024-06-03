46aa0624ceae903d60bb9c34af54bd542510e502
Joinemm <joonas@rautiola.co>
2024-06-03 15:06:16 +0300

    Refactor wezterm config

--- home-modules/wezterm.nix
+++ home-modules/wezterm.nix
@@ -2,38 +2,49 @@
   programs.wezterm = {
     enable = true;
     enableZshIntegration = true;
-    extraConfig = ''
-      local theme = "Dracula (Official)"
-      local scheme = wezterm.color.get_builtin_schemes()[theme]
-      scheme.background = "#101116"
+    extraConfig =
+      /*
+      lua
+      */
+      ''
+        local theme = "Dracula (Official)"
+        local scheme = wezterm.color.get_builtin_schemes()[theme]
+        scheme.background = "#101116"
 
-      return {
-        hide_tab_bar_if_only_one_tab = true,
-        font = wezterm.font_with_fallback({
-          "Fira Code Nerd Font",
-          "Twemoji",
-          "Twitter Color Emoji",
-          "Symbols Nerd Font",
-        }),
-        front_end = "WebGpu",
-        visual_bell = {
-          fade_in_duration_ms = 75,
-          fade_out_duration_ms = 75,
-          target = "CursorColor",
-        },
-        audible_bell = "Disabled",
-        font_size = 10.0,
-        color_schemes = {
-          [theme] = scheme,
-        },
-        color_scheme = theme,
-        window_background_opacity = 0.8,
-        keys = { {
-          mods = "CTRL|SHIFT",
-          key = "Enter",
-          action = wezterm.action.SpawnWindow,
-        } },
-      }
-    '';
+        return {
+          front_end = "WebGpu",
+          audible_bell = "Disabled",
+          font_size = 10.0,
+          window_background_opacity = 0.85,
+          hide_tab_bar_if_only_one_tab = true,
+
+          font = wezterm.font_with_fallback({
+            "Fira Code Nerd Font",
+            "Twemoji",
+            "Twitter Color Emoji",
+            "Symbols Nerd Font",
+          }),
+
+
+          visual_bell = {
+            fade_in_duration_ms = 75,
+            fade_out_duration_ms = 75,
+            target = "CursorColor",
+          },
+
+          color_scheme = theme,
+          color_schemes = {
+            [theme] = scheme,
+          },
+
+          keys = {
+            {
+              key = "Enter",
+              mods = "CTRL|SHIFT",
+              action = wezterm.action.SpawnWindow,
+            },
+          },
+        }
+      '';
   };
 }
