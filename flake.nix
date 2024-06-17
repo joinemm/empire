407506325c7c79f1eac0682384398a676d391bf5
Joinemm <joonas@rautiola.co>
2024-06-17 17:11:19 +0300

    Forget about nix-fast-build

--- flake.nix
+++ flake.nix
@@ -77,12 +77,6 @@
       };
     };
 
-    checks.x86_64-linux =
-      nixpkgs.lib.mapAttrs' (
-        name: config: nixpkgs.lib.nameValuePair name config.config.system.build.toplevel
-      )
-      self.nixosConfigurations;
-
     formatter.x86_64-linux =
       treefmt-nix.lib.mkWrapper
       nixpkgs.legacyPackages.x86_64-linux
