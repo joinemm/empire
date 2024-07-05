b7d783b54492e5507ae3bfe9ff2fb0e256592a7c
Joonas Rautiola <joonas@rautiola.co>
2024-07-05 11:36:27 +0300

    Update rpi4 config, make sdImage buildable

--- hosts/archimedes/configuration.nix
+++ hosts/archimedes/configuration.nix
@@ -3,6 +3,8 @@
   modules,
   inputs,
   pkgs,
+  user,
+  modulesPath,
   ...
 }: {
   imports = lib.flatten [
@@ -12,23 +14,39 @@
       ssh-access
     ])
     inputs.nixos-hardware.nixosModules.raspberry-pi-4
+    (modulesPath + "/installer/sd-card/sd-image-aarch64.nix")
   ];
 
-  hardware.raspberry-pi."4" = {
-    fkms-3d.enable = true;
-    touch-ft5406.enable = true;
+  nixpkgs.overlays = [
+    (_: super: {
+      makeModulesClosure = x:
+        super.makeModulesClosure (x // {allowMissing = true;});
+    })
+  ];
+
+  # makes the sdImage a .img instead of .img.zst
+  sdImage.compressImage = false;
+
+  hardware = {
+    raspberry-pi."4" = {
+      apply-overlays-dtmerge.enable = true;
+    };
+    deviceTree = {
+      enable = true;
+      filter = "*rpi-4-*.dtb";
+    };
   };
 
   nixpkgs.hostPlatform = "aarch64-linux";
   networking.hostName = "archimedes";
 
-  boot = {
-    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
-    initrd.availableKernelModules = ["xhci_pci" "usbhid" "usb_storage"];
-    loader = {
-      grub.enable = false;
-      generic-extlinux-compatible.enable = true;
-    };
+  console.enable = false;
+
+  security.sudo.wheelNeedsPassword = false;
+
+  users.users.${user.name} = {
+    shell = lib.mkForce pkgs.bashInteractive;
+    extraGroups = ["gpio"];
   };
 
   fileSystems = {
@@ -39,8 +57,19 @@
     };
   };
 
+  users.groups.gpio = {};
+
+  # Use GPIO pins as non-root
+  services.udev.extraRules = ''
+    SUBSYSTEM=="bcm2835-gpiomem", KERNEL=="gpiomem", GROUP="gpio",MODE="0660"
+    SUBSYSTEM=="gpio", KERNEL=="gpiochip*", ACTION=="add", RUN+="${pkgs.bash}/bin/bash -c 'chown root:gpio /sys/class/gpio/export /sys/class/gpio/unexport ; chmod 220 /sys/class/gpio/export /sys/class/gpio/unexport'"
+    SUBSYSTEM=="gpio", KERNEL=="gpio*", ACTION=="add",RUN+="${pkgs.bash}/bin/bash -c 'chown root:gpio /sys%p/active_low /sys%p/direction /sys%p/edge /sys%p/value ; chmod 660 /sys%p/active_low /sys%p/direction /sys%p/edge /sys%p/value'"
+  '';
+
   environment.systemPackages = with pkgs; [
     busybox
+    libraspberrypi
+    raspberrypi-eeprom
   ];
 
   system.stateVersion = "24.05";
