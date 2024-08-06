55528e655ee23532d35e294ca54052c310bfda13
Joonas Rautiola <joonas@rautiola.co>
2024-08-06 16:45:37 +0300

    Add aarch64 checks back, but don't run them on x86

--- deployments.nix
+++ deployments.nix
@@ -1,7 +1,7 @@
 {self, ...}: let
   inherit (self.inputs) deploy-rs;
-in {
-  flake.deploy.nodes = {
+
+  x86 = {
     apollo = {
       hostname = "65.21.249.145";
       profiles.system = {
@@ -9,21 +9,31 @@ in {
         path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.apollo;
       };
     };
-
-    archimedes = {
-      hostname = "192.168.1.3";
-      profiles.system = {
-        user = "root";
-        path = deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.archimedes;
-      };
-    };
-
     monitoring = {
       hostname = "65.108.222.239";
       profiles.system = {
         user = "root";
         path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.monitoring;
       };
+    };
+  };
+
+  aarch64 = {
+    archimedes = {
+      hostname = "192.168.1.3";
+      remoteBuild = true;
+      profiles.system = {
+        user = "root";
+        path = deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.archimedes;
+      };
+    };
+  };
+in {
+  flake = {
+    deploy.nodes = x86 // aarch64;
+    checks = {
+      x86_64-linux = deploy-rs.lib.x86_64-linux.deployChecks {nodes = x86;};
+      aarch64-linux = deploy-rs.lib.aarch64-linux.deployChecks {nodes = aarch64;};
     };
   };
 }
