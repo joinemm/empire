8b1dd4791d111001eae4d9480d55003105824d57
Joonas Rautiola <joonas@rautiola.co>
2024-08-25 13:03:55 +0300

    Try making pipewire more stable

--- modules/desktop/sound.nix
+++ modules/desktop/sound.nix
@@ -1,24 +1,40 @@
 {pkgs, ...}: {
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
     pulse.enable = true;
 
     # https://www.reddit.com/r/linux/comments/1em8biv/psa_pipewire_has_been_halving_your_battery_life/
-    wireplumber.extraConfig."10-disable-camera.conf" = {
-      "wireplumber.profiles".main."monitor.libcamera" = "disabled";
+    wireplumber = {
+      enable = true;
+      extraConfig."10-disable-camera.conf" = {
+        "wireplumber.profiles".main."monitor.libcamera" = "disabled";
+      };
+    };
+
+    extraConfig.pipewire-pulse."92-low-latency" = {
+      "context.properties" = [
+        {
+          name = "libpipewire-module-protocol-pulse";
+          args = {};
+        }
+      ];
+      "pulse.properties" = {
+        "pulse.min.req" = "32/48000";
+        "pulse.default.req" = "32/48000";
+        "pulse.max.req" = "32/48000";
+        "pulse.min.quantum" = "32/48000";
+        "pulse.max.quantum" = "32/48000";
+      };
+      "stream.properties" = {
+        "node.latency" = "32/48000";
+        "resample.quality" = 1;
+      };
     };
   };
 
   security.rtkit.enable = true;
 
-  hardware.pulseaudio.daemon.config = {
-    default-sample-format = "float32le";
-  };
-
   environment.systemPackages = with pkgs; [
-    alsa-utils
     pulseaudio
     playerctl
   ];
