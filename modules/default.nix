32d930e7ebde76ce9986d3327e6c0c8870010ccb
Joonas Rautiola <joonas@rautiola.co>
2024-09-11 15:27:25 +0300

    Remove unnecessary imports

--- modules/default.nix
+++ modules/default.nix
@@ -1,24 +1,24 @@
 # Reusable NixOS modules
 {
-  attic = import ./attic.nix;
-  bluetooth = import ./bluetooth.nix;
-  common = import ./common.nix;
-  desktop = import ./desktop;
-  docker = import ./docker.nix;
-  gaming = import ./gaming.nix;
-  home = import ./home.nix;
-  headless = import ./headless.nix;
-  hetzner = import ./hetzner.nix;
-  keyd = import ./keyd.nix;
-  laptop = import ./laptop.nix;
-  locale = import ./locale.nix;
-  nginx = import ./nginx.nix;
-  node-exporter = import ./node-exporter.nix;
-  remotebuild = import ./remotebuild.nix;
-  ssh-access = import ./ssh-access.nix;
-  syncthing = import ./syncthing.nix;
-  tailscale = import ./tailscale.nix;
-  transmission = import ./transmission.nix;
-  work = import ./work.nix;
-  yubikey = import ./yubikey.nix;
+  attic = ./attic.nix;
+  bluetooth = ./bluetooth.nix;
+  common = ./common.nix;
+  desktop = ./desktop;
+  docker = ./docker.nix;
+  gaming = ./gaming.nix;
+  home = ./home.nix;
+  headless = ./headless.nix;
+  hetzner = ./hetzner.nix;
+  keyd = ./keyd.nix;
+  laptop = ./laptop.nix;
+  locale = ./locale.nix;
+  nginx = ./nginx.nix;
+  node-exporter = ./node-exporter.nix;
+  remotebuild = ./remotebuild.nix;
+  ssh-access = ./ssh-access.nix;
+  syncthing = ./syncthing.nix;
+  tailscale = ./tailscale.nix;
+  transmission = ./transmission.nix;
+  work = ./work.nix;
+  yubikey = ./yubikey.nix;
 }
