7bedd0861301ad2cd6404a6c7300a3399c6d9d4f
Joonas Rautiola <joonas@rautiola.co>
2024-09-30 10:19:49 +0300

    Update hostnames in ssh config

--- home-modules/ssh-personal.nix
+++ home-modules/ssh-personal.nix
@@ -6,13 +6,9 @@
         hostname = "5.161.128.99";
         user = "root";
       };
-      andromeda = {
-        hostname = "65.21.184.54";
-        user = "captain";
-      };
-      apollo = {
-        hostname = "65.21.249.145";
-      };
+      alexandria.hostname = "65.21.249.145";
+      byzantium.hostname = " 65.108.222.239";
+      kyoto.hostname = "192.168.1.3";
     };
   };
 }
