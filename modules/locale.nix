18b7e18d2b92a4ecea98d9f9472e806f127950e8
Joonas Rautiola <joonas@rautiola.co>
2024-10-23 17:44:49 +0300

    Support japanese locale

--- modules/locale.nix
+++ modules/locale.nix
@@ -6,6 +6,11 @@
     extraLocaleSettings = {
       LC_MESSAGES = "en_US.UTF-8";
     };
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "fi_FI.UTF-8/UTF-8"
+      "ja_JP.UTF-8/UTF-8"
+    ];
   };
 
   location.provider = "geoclue2";
