1cfab19801879493be175013e25c76aa602edad3
Joonas Rautiola <joonas@rautiola.co>
2024-11-02 16:30:56 +0200

    Undervolt GPU, refactor config

---
+++ hosts/rome/5700XT.nix
@@ -0,0 +1,54 @@
+{ pkgs, lib, ... }:
+{
+  hardware.amdgpu = {
+    initrd.enable = true;
+    amdvlk = {
+      enable = true;
+      support32Bit.enable = true;
+      supportExperimental.enable = true;
+    };
+  };
+
+  # overclocking
+
+  environment.systemPackages = with pkgs; [
+    lact
+    unigine-heaven
+  ];
+
+  boot.kernelParams = [ "amdgpu.ppfeaturemask=0xfffd7fff" ];
+
+  systemd.services.lactd = {
+    wantedBy = [ "multi-user.target" ];
+    unitConfig = {
+      Description = "AMDGPU Control Daemon";
+      After = [ "multi-user.target" ];
+    };
+    serviceConfig = {
+      ExecStart = "${lib.getExe pkgs.lact} daemon";
+      Nice = -10;
+    };
+  };
+
+  # 2000 Mhz core clock
+  # 1800 Mhz memory clock
+  # 1100 mV max voltage
+  environment.etc."lact/config.yaml".text = # yaml
+    ''
+      daemon:
+        log_level: info
+        admin_groups:
+        - wheel
+        - sudo
+        disable_clocks_cleanup: false
+      apply_settings_timer: 5
+      gpus:
+        1002:731F-1DA2:E411-0000:0b:00.0:
+          fan_control_enabled: false
+          power_cap: 220.0
+          performance_level: auto
+          max_core_clock: 2000
+          max_memory_clock: 900
+          max_voltage: 1100
+    '';
+}
--- hosts/rome/default.nix
+++ hosts/rome/default.nix
@@ -24,6 +24,8 @@
     ])
     inputs.sops-nix.nixosModules.sops
     ./hardware-configuration.nix
+    ./5700XT.nix
+    ./monitor.nix
   ];
 
   system.stateVersion = "23.11";
@@ -38,35 +40,18 @@
     hostId = "c5a9072d";
   };
 
-  # latest ZFS compatible kernel
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_10;
-
-  services.xserver = {
-    # enable AMD Freesync
-    deviceSection = ''
-      Option "VariableRefresh" "true"
-    '';
-
-    xrandrHeads = [
-      {
-        # Force 144hz on primary display
-        output = "DisplayPort-0";
-        primary = true;
-        monitorConfig = ''
-          Modeline "3440x1440_144.00"  1086.75  3440 3744 4128 4816  1440 1443 1453 1568 -hsync +vsync
-          Option "PreferredMode" "3440x1440_144.00"
-        '';
-      }
-      {
-        # LG TV should be off by default.
-        output = "HDMI-A-0";
-        monitorConfig = ''
-          Option "Disable" "true"
-          Option "RightOf" "DisplayPort-0"
-        '';
-      }
+  fileSystems."${user.home}/nas" = {
+    device = "192.168.1.4:/";
+    fsType = "nfs";
+    options = [
+      "x-systemd.automount"
+      "noauto"
+      "users"
     ];
   };
+
+  # latest ZFS compatible kernel
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_10;
 
   # Sample rates for Topping D10 USB DAC
   services.pipewire.extraConfig = {
@@ -84,15 +69,6 @@
     };
   };
 
-  hardware.amdgpu = {
-    initrd.enable = true;
-    amdvlk = {
-      enable = true;
-      support32Bit.enable = true;
-      supportExperimental.enable = true;
-    };
-  };
-
   services.syncthing.settings.folders = {
     "camera".enable = true;
     "code".enable = true;
@@ -103,12 +79,6 @@
     "work".enable = true;
     "share".enable = true;
   };
-
-  # overclocking
-  boot.kernelParams = [ "amdgpu.ppfeaturemask=0xfffd7fff" ];
-  environment.systemPackages = with pkgs; [ lact ];
-  systemd.packages = with pkgs; [ lact ];
-  systemd.services.lact.enable = true;
 
   # extra home-manager configuration
   home-manager.users."${user.name}" = { };
